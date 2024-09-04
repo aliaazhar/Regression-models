80c8946c7057e597c2112ce951152e987fc2acdb
Azhar Ali <azhrali2018mp@gmail.com>
2024-09-05 03:43:24 +0530

    some besic changes

--- stuent_marks_pred.py
+++ stuent_marks_pred.py
@@ -1,8 +1,6 @@
-import numpy as np
+
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
@@ -10,14 +8,16 @@ import joblib
 
 # Load the data set using pd.read_csv in a variable called data
 data = pd.read_csv(r"C:\Users\azhar\Desktop\Data Sets\student_marks\Student_Marks.csv")
+
 # Check if there are any missing value in the dataset
 null_data=data.isnull().sum()
+print(null_data)
 
-
-#Check any duplicate values in the dataset
+# Check any duplicate values in the dataset
 duplicate_data=data.duplicated().sum()
+print(duplicate_data)
 
-# Check how many coursees get how much marks
+# Check the relationship between the variables
 grouped = data.groupby('number_courses')['Marks'].value_counts()
 
 # Visualize the relationship between the variables
@@ -26,14 +26,14 @@ fig = px.scatter(data,
                  y='Marks',
                  size = 'time_study',
                 trendline  = 'ols')
-# fig.show()
+fig.show()
 
 fig = px.scatter(data,
                 y = 'number_courses',
                 x = 'time_study',
                 size  = 'Marks',
                 trendline  = 'ols')
-# fig.show()
+fig.show()
 
 # Check the correlation between the variables
 correlation  = data.corr()
